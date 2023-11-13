99783736cea711910646ac2957e13633de44c4b5
Alcyone47 <91411013+Alcyone47@users.noreply.github.com>
2023-11-13 23:57:29 +0530

    splash-screen

--- kavach-prototype/src/pages/Home.jsx
+++ kavach-prototype/src/pages/Home.jsx
@@ -6,8 +6,19 @@ function Home() {
     const navigate = useNavigate();
     return (
         <div>
-            <h1>Home Page</h1>
+            {/* SplashScreen */}
+            <div id="splashscreen">
+                <h2>Welcome !</h2>
+                <img src="../assets/background.png"/>
+                <a href="#" class="enter_link">Enter on the website</a>
+            </div>
+
+            {/* Hero */}
+            <div>
+                <h1>Home Page</h1>
+            </div>
         </div>
+        
     )
 }
 
